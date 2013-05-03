229442b810dc1ef108661fdc7a46c69a809fa24b
jibi gopi <jibi@pluskb.com>
2013-05-03 11:15:19 +0530

    permission
    permission

--- application/views/edit_employee_permission.php
+++ application/views/edit_employee_permission.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 foreach ($edrow as $drow){
     echo $drow->first_name."<br>";
-    //echo $drow->image;
+     $drow->image;
     ?>
 <img src="<?php echo base_url()?>uploads/<?php echo $drow->image ?>">
 <?php
@@ -10,17 +10,12 @@ foreach ($edrow as $drow){
 }
 
 
-foreach ($erow as $eprow){
-    $per=$eprow->permission;
-}
-
 
 
 
 ?>
 
-  </style>
-</head>
+
 
 <body>
     <br><br>
@@ -42,21 +37,59 @@ foreach ($erow as $eprow){
  $delete= $num/1000%10;
 
 ?>
+            
 <tr><td><label>READ</label> </td>
-    <td><input type="radio" name="read" value="001" <?php if($read==1){ ?>checked<?php }?> > ON</td>
-    <td><input type="radio" name="read" value="000" <?php if($read==0) { ?>checked<?php }?> > OFF</td>
+    <td><input type="radio" name="read" value="0001" <?php if($read==1){ ?>checked<?php }?> > ON</td>
+    <td><input type="radio" name="read" value="0000" <?php if($read==0) { ?>checked<?php }?> > OFF</td>
 </tr>
 <tr><td><label>ADD</label> </td>
-    <td><input type="radio" name="add" value="on" <?php if($add==1){ ?>checked<?php }?> > ON</td>
-    <td><input type="radio" name="add" value="off" <?php if($add==0){ ?>checked<?php }?>> OFF</td>
+    <td><input type="radio" name="add" value="0010" <?php if($add==1){ ?>checked<?php }?> > ON</td>
+    <td><input type="radio" name="add" value="0000" <?php if($add==0){ ?>checked<?php }?>> OFF</td>
 </tr>
 <tr><td><label>EDIT</label> </td>
-    <td><input type="radio" name="edit" value="on"<?php if($edit==1){ ?>checked<?php }?>> ON</td>
-    <td><input type="radio" name="edit" value="off" <?php if($edit==0){ ?>checked<?php }?>> OFF</td>
+    <td><input type="radio" name="edit" value="0100"<?php if($edit==1){ ?>checked<?php }?>> ON</td>
+    <td><input type="radio" name="edit" value="0000" <?php if($edit==0){ ?>checked<?php }?>> OFF</td>
 </tr>
 <tr><td><label>DELETE</label> </td>
-    <td><input type="radio" name="delete" value="on" <?php if($delete==1 ){ ?>checked<?php }?>> ON</td>
-    <td><input type="radio" name="delete" value="off" <?php if($delete==0){ ?>checked<?php }?>> OFF</td>
+    <td><input type="radio" name="delete" value="1000" <?php if($delete==1 ){ ?>checked<?php }?>> ON</td>
+    <td><input type="radio" name="delete" value="0000" <?php if($delete==0){ ?>checked<?php }?>> OFF</td>
+</tr>
+
+        </table>
+        <table style="margin-left: 270px;margin-top: -145px">
+            <tr><td><label>FOR EMPLOYEE PERMISSION</label> </br></br></td></tr>
+           
+ <?php foreach ($erow as $eprow){
+    $per=$eprow->permission;
+}
+
+
+
+ $eread= $per%10;
+
+ $eadd= $per/10%10;
+
+ $eedit= $per/100%10;
+
+ $edelete= $per/1000%10;
+
+?>
+            
+<tr><td><label>READ</label> </td>
+    <td><input type="radio" name="eread" value="0001" <?php if($eread==1){ ?>checked<?php }?> > ON</td>
+    <td><input type="radio" name="eread" value="0000" <?php if($eread==0) { ?>checked<?php }?> > OFF</td>
+</tr>
+<tr><td><label>ADD</label> </td>
+    <td><input type="radio" name="eadd" value="0010" <?php if($eadd==1){ ?>checked<?php }?> > ON</td>
+    <td><input type="radio" name="eadd" value="0000" <?php if($eadd==0){ ?>checked<?php }?>> OFF</td>
+</tr>
+<tr><td><label>EDIT</label> </td>
+    <td><input type="radio" name="eedit" value="0100"<?php if($eedit==1){ ?>checked<?php }?>> ON</td>
+    <td><input type="radio" name="eedit" value="0000" <?php if($eedit==0){ ?>checked<?php }?>> OFF</td>
+</tr>
+<tr><td><label>DELETE</label> </td>
+    <td><input type="radio" name="edelete" value="1000" <?php if($edelete==1 ){ ?>checked<?php }?>> ON</td>
+    <td><input type="radio" name="edelete" value="0000" <?php if($edelete==0){ ?>checked<?php }?>> OFF</td>
 </tr>
 
         </table>
@@ -64,13 +97,3 @@ foreach ($erow as $eprow){
 </body>
 </html>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
