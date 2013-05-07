613c88b9edb05074f83b9ab2ed1c21e561d3f407
jibi gopi <jibi@pluskb.com>
2013-05-07 19:22:09 +0530

    start
    start

--- application/views/employee_list.php
+++ application/views/employee_list.php
@@ -33,5 +33,5 @@ foreach ($row as $erow){
 
 
 ?>  
-<tb><input type="submit" name="delete_all" value="Delete Selected Message"></td><tb><input type="submit" name="Add_employee" value="Add New Employee"></td><tb><input type="submit" name="edit_all" value="Send Email"></td><td><?php echo form_submit('BacktoHome','Back To Home') ?></td>
+<tb><input type="submit" name="delete_all" value="Delete Selected User"></td><tb><input type="submit" name="Add_employee" value="Add New Employee"></td><tb><input type="submit" name="edit_all" value="Send Email"></td><td><?php echo form_submit('BacktoHome','Back To Home') ?></td>
      <?php echo form_close() ?> 
