1aebf56533713073a235596b39c7e0d1cc365988
jibi gopi <jibi@pluskb.com>
2013-05-13 14:44:50 +0530

    start
    start

--- application/models/setting.php
+++ application/models/setting.php
@@ -1,5 +1,11 @@
 <?php
 class Setting extends CI_Model{
-    
+    function __construct() {
+        parent::__construct();
+    }
+    function get_branch_setting(){
+        $this->db->select()->from('settings');
+        $sql=$this->db->get();
+    }
 }
 ?>
