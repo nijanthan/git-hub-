81569d27ac632fb2bab345a9f7b5626f04b68bf8
jibi gopi <jibi@pluskb.com>
2013-05-13 16:56:44 +0530

    start
    start

--- databse/pos.sql
+++ databse/pos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 07, 2013 at 12:43 PM
+-- Generation Time: May 13, 2013 at 11:26 AM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.4.3
 
@@ -56,6 +56,47 @@ INSERT INTO `branch` (`id`, `store_name`, `store_place`, `store_city`, `store_zi
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `branch_per`
+--
+
+CREATE TABLE IF NOT EXISTS `branch_per` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `permission` varchar(11) NOT NULL,
+  `depart_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `branch_per`
+--
+
+INSERT INTO `branch_per` (`id`, `permission`, `depart_id`, `branch_id`) VALUES
+(12, '1111', 17, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `depabranch`
+--
+
+CREATE TABLE IF NOT EXISTS `depabranch` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `branch_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data for table `depabranch`
+--
+
+INSERT INTO `depabranch` (`id`, `branch_id`, `department_id`) VALUES
+(16, 1, 17);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `department`
 --
 
@@ -63,21 +104,35 @@ CREATE TABLE IF NOT EXISTS `department` (
   `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
   `dep_name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
 -- Dumping data for table `department`
 --
 
 INSERT INTO `department` (`id`, `dep_name`) VALUES
-(1, 'Sales Man'),
-(2, 'Stock Man'),
-(3, 'Office Supplies'),
-(4, 'Support Managers'),
-(5, 'Sales Support '),
-(6, 'Marketing Office'),
-(7, 'Store Main'),
-(8, 'Art');
+(17, 'Sales Man');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `depart_per`
+--
+
+CREATE TABLE IF NOT EXISTS `depart_per` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `permission` varchar(11) NOT NULL,
+  `depart_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `depart_per`
+--
+
+INSERT INTO `depart_per` (`id`, `permission`, `depart_id`, `branch_id`) VALUES
+(12, '1111', 17, 1);
 
 -- --------------------------------------------------------
 
@@ -90,27 +145,14 @@ CREATE TABLE IF NOT EXISTS `employeepermission` (
   `permission` varchar(100) NOT NULL,
   `emp_id` int(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
 
 --
 -- Dumping data for table `employeepermission`
 --
 
 INSERT INTO `employeepermission` (`id`, `permission`, `emp_id`) VALUES
-(13, '0001', 28),
-(14, '0000', 29),
-(15, '0000', 30),
-(16, '0000', 32),
-(17, '0000', 33),
-(18, '0000', 34),
-(19, '0000', 35),
-(20, '0000', 36),
-(21, '0000', 40),
-(22, '0000', 42),
-(23, '0000', 43),
-(24, '1111', 45),
-(25, '0000', 46),
-(26, '0000', 47);
+(34, '1111', 55);
 
 -- --------------------------------------------------------
 
@@ -123,27 +165,55 @@ CREATE TABLE IF NOT EXISTS `itempermission` (
   `permission` varchar(100) NOT NULL,
   `emp_id` int(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
 
 --
 -- Dumping data for table `itempermission`
 --
 
 INSERT INTO `itempermission` (`id`, `permission`, `emp_id`) VALUES
-(12, '1001', 28),
-(13, '0000', 29),
-(14, '0000', 30),
-(15, '0000', 32),
-(16, '0000', 33),
-(17, '0000', 34),
-(18, '0000', 35),
-(19, '0000', 36),
-(20, '0000', 40),
-(21, '0000', 42),
-(22, '0000', 43),
-(23, '1111', 45),
-(24, '0000', 46),
-(25, '0000', 47);
+(34, '1111', 55);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_per`
+--
+
+CREATE TABLE IF NOT EXISTS `item_per` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission` varchar(11) NOT NULL,
+  `depart_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `item_per`
+--
+
+INSERT INTO `item_per` (`id`, `permission`, `depart_id`, `branch_id`) VALUES
+(12, '1111', 17, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `department` int(11) NOT NULL,
+  `branch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `department`, `branch`) VALUES
+(1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -157,18 +227,16 @@ CREATE TABLE IF NOT EXISTS `userbranchs` (
   `branch_name` varchar(100) NOT NULL,
   `emp_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=66 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=91 ;
 
 --
 -- Dumping data for table `userbranchs`
 --
 
 INSERT INTO `userbranchs` (`id`, `branch_id`, `branch_name`, `emp_id`) VALUES
-(50, 4, 'ponkunnam', 45),
-(51, 1, 'HSR Layout', 45),
-(63, 2, 'Erumely', 47),
-(64, 1, 'HSR Layout', 47),
-(65, 4, 'ponkunnam', 47);
+(88, 1, 'HSR Layout', 55),
+(89, 4, 'ponkunnam', 55),
+(90, 3, 'Kottayam', 55);
 
 -- --------------------------------------------------------
 
@@ -182,7 +250,7 @@ CREATE TABLE IF NOT EXISTS `userdepart` (
   `depart_name` varchar(100) NOT NULL,
   `emp_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
 
 --
 -- Dumping data for table `userdepart`
@@ -192,14 +260,22 @@ INSERT INTO `userdepart` (`id`, `depart_id`, `depart_name`, `emp_id`) VALUES
 (22, 1, 'Sales Man', 45),
 (23, 7, 'Store Main', 45),
 (24, 5, 'Sales Support ', 45),
-(35, 1, 'Sales Man', 47),
-(36, 6, 'Marketing Office', 47),
-(37, 5, 'Sales Support ', 47),
-(38, 8, 'Art', 47),
-(39, 7, 'Store Main', 47),
-(40, 4, 'Support Managers', 47),
-(41, 3, 'Office Supplies', 47),
-(42, 2, 'Stock Man', 47);
+(43, 1, 'Sales Man', 47),
+(44, 5, 'Sales Support ', 47),
+(45, 2, 'Stock Man', 47),
+(54, 1, 'Sales Man', 48),
+(55, 1, 'Sales Man', 49),
+(56, 4, 'Support Managers', 49),
+(57, 1, 'Sales Man', 50),
+(58, 3, 'Office Supplies', 50),
+(59, 6, 'Marketing Office', 50),
+(60, 3, 'Office Supplies', 51),
+(61, 5, 'Sales Support ', 51),
+(62, 1, 'Sales Man', 52),
+(63, 4, 'Support Managers', 52),
+(64, 4, 'Support Managers', 53),
+(65, 3, 'Office Supplies', 54),
+(66, 17, 'Sales Man', 55);
 
 -- --------------------------------------------------------
 
@@ -229,15 +305,35 @@ CREATE TABLE IF NOT EXISTS `users` (
   `deleted_by` int(100) NOT NULL,
   `deleted` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=48 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=56 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `user_id`, `password`, `first_name`, `last_name`, `address`, `sex`, `age`, `city`, `state`, `zip`, `country`, `email`, `phone`, `image`, `dob`, `active`, `created_by`, `deleted_by`, `deleted`) VALUES
-(45, 'Usd125', 'Reset@123', 'monish', 'km', 'Slvpg', 'Male', 23, 'bangalore', 'karnadaka', '675646', 'india', 'monish@gmail.com', '7795398584', '1322524800', '0', 0, 28, 0, 0),
-(47, 'USD123', 'Reset@123', 'jibi', 'gopi', 'slvpg', 'Male', 23, 'bangalore', 'karandaka', '675643', 'india', 'jibi344443@yahoo.com', '7795398584', '654912000', '0', 0, 45, 0, 0);
+(55, 'Usd123', 'f91e15dbec69fc40f81f0876e7009648', 'jibi', 'gopi', 'slvpg', 'Male', 23, 'bangalore', 'karnada', '676809', 'india', 'jibi344443@yahoo.com', '7795398584', '10.jpg', '654739200', 0, 45, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_per`
+--
+
+CREATE TABLE IF NOT EXISTS `user_per` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `permission` varchar(11) NOT NULL,
+  `depart_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `user_per`
+--
+
+INSERT INTO `user_per` (`id`, `permission`, `depart_id`, `branch_id`) VALUES
+(12, '1111', 17, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
