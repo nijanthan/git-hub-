6b4b49318e638d1038636d51463dea89b22739bd
jibi gopi <jibi@pluskb.com>
2013-05-13 14:30:42 +0530

    start

---
+++ application/logs/log-2013-05-13.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-05-13 04:42:15 --> Query error: Duplicate entry '0' for key 'PRIMARY'
+ERROR - 2013-05-13 06:47:19 --> Query error: Unknown column '13' in 'where clause'
+ERROR - 2013-05-13 06:48:11 --> Query error: Unknown column '13' in 'where clause'
+ERROR - 2013-05-13 06:48:14 --> Query error: Unknown column '13' in 'where clause'
+ERROR - 2013-05-13 07:05:40 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\PointOfSale\application\views\department.php 5
+ERROR - 2013-05-13 07:05:41 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\PointOfSale\application\views\department.php 5
---
+++ application/models/permissions.php
@@ -0,0 +1,36 @@
+<?php
+
+class Permissions extends CI_Model{
+    function __construct() {
+        parent::__construct();
+    }
+    function set_item_permission($item,$depart_id,$branch_id){
+        $data=array('permission'=>$item,
+                    'depart_id'=>$depart_id,
+                    'branch_id'=>$branch_id);
+        $this->db->insert('item_per',$data);
+        
+    }
+    function set_user_permission($item,$depart_id,$branch_id){
+        $data=array('permission'=>$item,
+                    'depart_id'=>$depart_id,
+                    'branch_id'=>$branch_id);
+        $this->db->insert('user_per',$data);
+        
+    }
+    function set_depart_permission($item,$depart_id,$branch_id){
+        $data=array('permission'=>$item,
+                    'depart_id'=>$depart_id,
+                    'branch_id'=>$branch_id);
+        $this->db->insert('depart_per',$data);
+        
+    }
+     function set_branch_permission($item,$depart_id,$branch_id){
+        $data=array('permission'=>$item,
+                    'depart_id'=>$depart_id,
+                    'branch_id'=>$branch_id);
+        $this->db->insert('branch_per',$data);
+        
+    }
+}
+?>
